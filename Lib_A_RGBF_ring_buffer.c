28da83fd5046e2b87e75e3578c91301dcc741fc8
Isaev Mickle <mrraptor26@gmail.com>
2017-10-15 23:43:07 +0300

    Рефакторинг имен функций и переменных, алгоритм работы не затронут.

--- Lib_A_RGBF_ring_buffer.c
+++ Lib_A_RGBF_ring_buffer.c
@@ -35,84 +35,84 @@
 
 /**
  *  @brief  Функция выполняет запись байта информации в кольцевой буфер;
- *  @param  *ringBuff:  Указатель на структуру, содержащую указатели для работы 
- *                      с кольцевым буфером;
- *  @param  *value: Указатель на байты данных, которые необходимо записать в кольцевой буфер;
- *  @param  bytesNumb:  Количество байт, которое необходимо записать в кольцевой буфер
+ *  @param  *pBufStruct:    Указатель на структуру, содержащую указатели для
+ *                          работы с кольцевым буфером;
+ *  @param  *pByteToWriteInBuf: Указатель на байты данных, которые необходимо 
+ *                              записать в кольцевой буфер;
+ *  @param  lenght: Количество байт, которое необходимо записать в кольцевой буфер;
+ *  @warning    Следует первое условие "if (pBufPointsStruct->pWriteDataInBuf == pBufPointsStruct->endBuf)"
+ *             заменить "if (pBufPointsStruct->pWriteDataInBuf <= pBufPointsStruct->endBuf)" а крайнее else убрать;
  */
-void RGBF_RingBuff_In(RGBF_ring_buf_s *ringBuff,
-                      void *pValue,
-                      size_t bytesNumb)
+void RGBF_CopyDataInBuf(RGBF_ring_buf_s *pBufStruct,
+                        void *pByteToWriteInBuf,
+                        size_t lenght)
 {
     size_t i;
-    for (i = 0; i < bytesNumb; i++)
+    for (i = 0; i < lenght; i++)
     {
-        if (ringBuff->pWrite == ringBuff->endBuff) //   Если указатель указывает на 
-            //                                          адрес конца буфера;
+        //  Если указатель указывает на адрес конца буфера:
+        if (pBufStruct->pWriteDataInBuf == pBufStruct->pEndBuf)
         {
-
-            *ringBuff->pWrite++ = *(char*) pValue++; // Записываем по указанному адресу 
-            //                                          (в конец буфера) данные и инкрементируем 
-            //                                          указатель на переменную, которую 
-            //                                          необходимо записать в буфер;  
+            //  Записываем по указанному адресу (в конец буфера) данные и 
+            //  инкрементируем указатели;           
+            *pBufStruct->pWriteDataInBuf++ = *(char*) pByteToWriteInBuf++;
         }
-        else if (ringBuff->pWrite > ringBuff->endBuff) //   Если "вылетели" за 
-            //                                          пределы массива буфера
+            //  Если "вылетели" за пределы массива буфера:
+        else if (pBufStruct->pWriteDataInBuf > pBufStruct->pEndBuf)
         {
-            ringBuff->pWrite = ringBuff->startBuff; //  Переносим указатель на адрес начала буфера;  
-            *ringBuff->pWrite++ = *(char*) pValue++; // Записываем по указанному адресу 
-            //                                          данные и инкрементируем указатель на то, 
-            //                                          куда будем записывать данные в 
-            //                                          следующей итерации;
+            //  Переносим указатель на адрес начала буфера;  
+            pBufStruct->pWriteDataInBuf = pBufStruct->pBeginBuf;
+
+            //  Записываем по указанному адресу (в конец буфера) данные и 
+            //  инкрементируем указатели;       
+            *pBufStruct->pWriteDataInBuf++ = *(char*) pByteToWriteInBuf++;
         }
         else
         {
-            *ringBuff->pWrite++ = *(char*) pValue++; // Записываем по указанному адресу 
-            //                                          данные и инкрементируем адрес буфера;
+            //  Записываем по указанному адресу (в конец буфера) данные и 
+            //  инкрементируем указатели;        
+            *pBufStruct->pWriteDataInBuf++ = *(char*) pByteToWriteInBuf++;
         }
     }
 }
 
 /**
- *  @brief  Функция выполяет чтение данных из кольцевого буфера;
- *  @param  *ringBuff:  Указатель на структуру, содержащую указатели для работы 
- *                      с кольцевым буфером;
- *  @param  *pArr:  Указатель на массив данных, куда необходимо скопировать данные 
- *                  из кольцевого буфера;
- *  @param  lenght: Количество байт, которое необходимо скопировать из кольцевого буфера;
+ *  @brief  Функция выполяет копирование данных из кольцевого буфера;
+ *  @param  *pBufStruct:    Указатель на структуру, содержащую указатели для работы 
+ *                         с кольцевым буфером;
+ *  @param  *pArrToWriteFromBuf:    Указатель на массив данных, куда необходимо 
+ *                                  скопировать данные  из кольцевого буфера;
+ *  @param  lenght: Количество байт, которое необходимо скопировать из кольцевого 
+ *                  буфера;
  */
-void RGBF_RingBuff_Out(RGBF_ring_buf_s *ringBuff,
-                       void *pArr,
-                       size_t lenght)
+void RGBF_CopyDataOutBuf(RGBF_ring_buf_s *pBufStruct,
+                         void *pArrToWriteFromBuf,
+                         size_t lenght)
 {
     size_t i;
     for (i = 0; i < lenght; i++)
     {
-        if (ringBuff->pRead == ringBuff->endBuff) //    Если указатель указывает 
-            //                                          на адрес конца буфера;
+        //  //  Если указатель указывает на адрес конца буфера:
+        if (pBufStruct->pReadDataFromBuf == pBufStruct->pEndBuf)
         {
-            *(char*) pArr++ = *ringBuff->pRead++; //      Записываем в массив данные из буфера, 
-            //                                  находящиеся по указанному адресу и 
-            //                                  инкрементируем адрес массива, 
-            //                                  в который записали данные;           
+            //  Записываем по указанному адресу данные из буфера и инкрементируем 
+            //  указатели;
+            *(char*) pArrToWriteFromBuf++ = *pBufStruct->pReadDataFromBuf++;
         }
-        else if (ringBuff->pRead > ringBuff->endBuff) //    Если "вылетели" за 
-            //                                              пределы массива буфера
+            //  Если "вылетели" за пределы массива буфера:
+        else if (pBufStruct->pReadDataFromBuf > pBufStruct->pEndBuf)
         {
-            ringBuff->pRead = ringBuff->startBuff; //   Переносим указатель на адрес начала буфера;
-            *(char*) pArr++ = *ringBuff->pRead++; //    Записываем в массив данные из 
-            //                                      буфера и инкрементируем указатель 
-            //                                      на массив в который записываем 
-            //                                      данные и указатель на то, откуда 
-            //                                      считали данные буфера;
+            //  Переносим указатель на адрес начала буфера;  
+            pBufStruct->pReadDataFromBuf = pBufStruct->pBeginBuf;
+            //  Записываем по указанному адресу данные из буфера и инкрементируем 
+            //  указатели;
+            *(char*) pArrToWriteFromBuf++ = *pBufStruct->pReadDataFromBuf++;
         }
         else
         {
-            *(char*) pArr++ = *ringBuff->pRead++; //    Записываем в массив данные из 
-            //                                      буфера и инкрементируем указатель 
-            //                                      на массив в который записываем 
-            //                                      данные и указатель на то, откуда 
-            //                                      считали данные буфера;
+            //  Записываем по указанному адресу данные из буфера и инкрементируем 
+            //  указатели;
+            *(char*) pArrToWriteFromBuf++ = *pBufStruct->pReadDataFromBuf++;
         }
     }
 }
@@ -120,43 +120,45 @@ void RGBF_RingBuff_Out(RGBF_ring_buf_s *ringBuff,
 /**
  *  @brief  Функция проверяет количество байт в кольцевом буфере, которое не было 
  *          скопировано;
- *  @param  *ringBuff:  Указатель на структуру, содержащую указатели для работы 
- *                      с кольцевым буфером;
- *  @return Количество необработаных байт кольцевого буфера;
+ *  @param  *pBufStruct:    Указатель на структуру, содержащую указатели для работы 
+ *                          с кольцевым буфером;
+ *  @return Количество необработаных байт в кольцевом буфере;
  */
-size_t RGBF_RingBuffCheckDataCnt(RGBF_ring_buf_s *ringBuff)
+size_t RGBF_num_CheckDataNum(RGBF_ring_buf_s *pBufStruct)
 {
-    size_t temp1, temp2;
-    if (ringBuff->pWrite >= ringBuff->pRead) // Если указатель на запись больше 
-        //                                      чем указатель на чтение;
+
+    //  Если указатель на запись больше чем указатель на чтение:
+    if (pBufStruct->pWriteDataInBuf >= pBufStruct->pReadDataFromBuf)
     {
-        return (ringBuff->pWrite - ringBuff->pRead);
+        return (pBufStruct->pWriteDataInBuf - pBufStruct->pReadDataFromBuf);
     }
-    else if (ringBuff->pWrite == ringBuff->pRead) //    Если указатель на запись 
-        //  данных в массив совпадает с указателем на чтение данных из массива;
+        //  Если указатель на запись данных в массив совпадает с указателем на 
+        //  чтение данных из массива:
+    else if (pBufStruct->pWriteDataInBuf == pBufStruct->pReadDataFromBuf)
     {
         return 0;
     }
-    else // Если указатель на запись меньше чем указатель на чтение (т.е. указатель 
-        //  на запись "перешагнул" через конец буфера);
+        // Если указатель на запись меньше чем указатель на чтение (т.е. указатель 
+        // на запись "перешагнул" через конец буфера):
+    else
     {
-        temp1 = ringBuff->endBuff - ringBuff->pRead;
-        temp2 = ringBuff->pRead - ringBuff->pWrite;
-        return (temp1 + temp2);
+        size_t bytesNum1, BytesNum2;
+        bytesNum1 = pBufStruct->pEndBuf - pBufStruct->pReadDataFromBuf;
+        BytesNum2 = pBufStruct->pReadDataFromBuf - pBufStruct->pWriteDataInBuf;
+        return (bytesNum1 + BytesNum2);
     }
 }
 
 /**
  *  @brief  Функция устанавливает указатели чтения и записи кольцевого буфера в 
  *          начало массива кольцевого буфера;
- *  @param  *ringBuff:  Указатель на структуру, содержащую указатели для работы 
- *                      с кольцевым буфером;
- *  @param  *pArr:  Указатель на 0-й элемент массива кольцевого буфера;
+ *  @param  *pBufStruct:    Указатель на структуру, содержащую указатели для работы 
+ *                          с кольцевым буфером;
  */
-void RGBF_ResetRingBuff(RGBF_ring_buf_s *ringBuff)
+void RGBF_ResetPointersToBegin(RGBF_ring_buf_s *pBufStruct)
 {
-    ringBuff->pRead = ringBuff->startBuff;
-    ringBuff->pWrite = ringBuff->startBuff;
+    pBufStruct->pReadDataFromBuf = pBufStruct->pBeginBuf;
+    pBufStruct->pWriteDataInBuf = pBufStruct->pBeginBuf;
 }
 //==============================================================================
 //******************************************************************************
